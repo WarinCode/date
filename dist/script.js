24b79308cb2651b1a1cfd04e3625e0ab4c1caba4
Varin Saipanya <94356640+Varin471@users.noreply.github.com>
2022-04-27 14:47:12 +0700

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1,14 +1,11 @@
-// "use strict";
-// class today {
-//     constructor(date) {
-//         this.date = date;
-//         this.day = this.date.getDate();
-//         this.month = this.date.getMonth() + 1;
-//         this.year = this.date.getFullYear();
-//     }
-//     ;
-// }
-// ;
-// const Today = new today(new Date());
-// console.log(`${Today.day}/${Today.month}/${Today.year} `);
-// console.log(`วันนี้วันที่ ${Today.day} เดือน ${Today.month} ปี ${Today.year} `);
+var today = /** @class */ (function() {
+    function today(date) {
+        this.date = date;
+        this.day = this.date.getDate();
+        this.month = this.date.getMonth() + 1;
+        this.year = this.date.getFullYear();
+    };
+    return today;
+}());;
+var Today = new today(new Date());
+console.log("".concat(Today.day, "/").concat(Today.month, "/").concat(Today.year, " "));
